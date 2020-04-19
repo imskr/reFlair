ea0347247069c0c18741f50baa42257f1d802b51
Shubham Kumar <shubhamkrai123@gmail.com>
2020-04-19 20:42:10 +0530

    change layout

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-// elements
+// menu items
 var $page = $('.page');
 
 $('.menu_toggle').on('click', function(){
@@ -7,3 +7,18 @@ $('.menu_toggle').on('click', function(){
 $('.content').on('click', function(){
   $page.removeClass('shazam');
 });
+
+// search box
+function searchToggle(obj, evt){
+  var container = $(obj).closest('.search-wrapper');
+      if(!container.hasClass('active')){
+          container.addClass('active');
+          evt.preventDefault();
+      }
+      else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+          container.removeClass('active');
+          container.removeClass('')
+          // clear input
+          container.find('.search-input').val('');
+      }
+}
